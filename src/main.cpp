f73b737715fb2fc4ab3ad3b3655314d4f92de2d6
unknown <dsirotenko@miem.hse.ru>
2020-06-10 18:31:26 +0300

    final

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ int main() {
     for (int i = 1; i <= 8; i++)
       for (int j = 1; j <= 8; j++) {
         if (grid[i][j].kind == grid[i + 1][j].kind)
-        if (grid[i][j].kind == grid[i - 1][j].kind)
+          if (grid[i][j].kind == grid[i - 1][j].kind)
             for (int n = -1; n <= 1; n++)
               grid[i + n][j].match++;
 
@@ -141,7 +141,7 @@ int main() {
           if (grid[i][j].match) {
             grid[i][j].kind = rand() % 7;
             grid[i][j].y = -ts * n++;
-            grid[i][y].match = 0;
+            grid[i][j].match = 0;
             grid[i][j].alpha = 255;
           }
     }
